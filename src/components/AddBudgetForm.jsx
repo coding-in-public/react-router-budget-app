1ae95bf9260d8a3941e848746a1b21595719f1cd
Chris Pennington <chris@codinginpublic.dev>
2023-02-06 15:06:34 -0700

    lesson-7

---
+++ src/components/AddBudgetForm.jsx
@@ -0,0 +1,46 @@
+import { Form } from "react-router-dom"
+
+// library imports
+import { CurrencyDollarIcon } from "@heroicons/react/24/solid"
+
+const AddBudgetForm = () => {
+  return (
+    <div className="form-wrapper">
+      <h2 className="h3">
+        Create budget
+      </h2>
+      <Form
+        method="post"
+        className="grid-sm"
+      >
+        <div className="grid-xs">
+          <label htmlFor="newBudget">Budget Name</label>
+          <input
+            type="text"
+            name="newBudget"
+            id="newBudget"
+            placeholder="e.g., Groceries"
+            required
+          />
+        </div>
+        <div className="grid-xs">
+          <label htmlFor="newBudgetAmount">Amount</label>
+          <input
+            type="number"
+            step="0.01"
+            name="newBudgetAmount"
+            id="newBudgetAmount"
+            placeholder="e.g., $350"
+            required
+            inputMode="decimal"
+          />
+        </div>
+        <button type="submit" className="btn btn--dark">
+          <span>Create budget</span>
+          <CurrencyDollarIcon width={20} />
+        </button>
+      </Form>
+    </div>
+  )
+}
+export default AddBudgetForm
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 
 // components
 import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 //  helper functions
 import { fetchData } from "../helpers"
@@ -13,7 +14,8 @@ import { fetchData } from "../helpers"
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName }
+  const budgets = fetchData("budgets");
+  return { userName, budgets }
 }
 
 // action
@@ -29,11 +31,23 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData()
+  const { userName, budgets } = useLoaderData()
 
   return (
     <>
-      {userName ? (<p>{userName}</p>) : <Intro />}
+      {userName ? (
+        <div className="dashboard">
+          <h1>Welcome back, <span className="accent">{userName}</span></h1>
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : <Intro />}
     </>
   )
 }
