3174f3c4a3bdcec5dcf6a787ea4ed68499e009dc
Chris Pennington <chris@codinginpublic.dev>
2023-02-06 14:49:46 -0700

    lesson-6

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,7 +21,8 @@ export async function dashboardAction({ request }) {
   const data = await request.formData();
   const formData = Object.fromEntries(data)
   try {
-    throw new Error('Ya done')
+    localStorage.setItem("userName", JSON.stringify(formData.userName))
+    return toast.success(`Welcome, ${formData.userName}`)
   } catch (e) {
     throw new Error("There was a problem creating your account.")
   }
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,32 @@
+import { useRouteError, Link, useNavigate } from "react-router-dom"
+
+// library imports
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+
 const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Error
+    <div className="error">
+      <h1>Uh oh! We’ve got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button
+          className="btn btn--dark"
+          onClick={() => navigate(-1)}
+        >
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go Back</span>
+        </button>
+        <Link
+          to="/"
+          className="btn btn--dark"
+        >
+          <HomeIcon width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
     </div>
   )
 }
